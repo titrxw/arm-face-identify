a88ebecf210eae44f7f657d49f8c53389a873b5e
titrxw <1120309488@qq.com>
2022-07-19 22:11:05 +0800

    update

--- app/Face/Train.h
+++ app/Face/Train.h
@@ -2,9 +2,10 @@
 // Created by rxwyun on 2022/7/13.
 //
 
-#ifndef ARM_FACE_IDENTIFY_TRAIN_H
-#define ARM_FACE_IDENTIFY_TRAIN_H
+#ifndef ARM_FACE_IDENTIFY_TRAIN_APP_H
+#define ARM_FACE_IDENTIFY_TRAIN_APP_H
 
+#include "mqtt/async_client.h"
 #include "../../core/train/Include/DialogVideoFaceTrain.h"
 
 class Train : public ArmFaceIdentify::DialogVideoFaceTrain {
